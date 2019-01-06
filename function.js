c3c5e294760cef19148d4797cc79e2d5ae51f45f
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-01-06 17:37:03 -0500

    added ability to fill page based on nav button click

--- function.js
+++ function.js
@@ -1,1 +1,66 @@
-alert("Hello")
+// window.onload = pages;
+//
+// function pages(){
+//
+//   // document.getElementById('portfolio').addEventListener("click", displayPortfolio);
+//   document.getElementById('portfolio').addEventListener("click", displayFetchPortfolio);
+//   document.getElementById('publications').addEventListener("click", displayPublications);
+//   document.getElementById('blog').addEventListener("click", displayBlog);
+//   document.getElementById('about').addEventListener("click", displayAbout);
+//   document.getElementById('title').addEventListener("click", displayHome);
+//
+//   function displayFetchPortfolio(e){
+//     // alert("Portfolio")
+//     e.preventDefault();
+//     main = document.getElementById('main')
+//     main.innerHTML = "";
+//     main.innerHTML = "<h1>Hello</h1>";
+//     // main.style.cssText = "color: blue; border: 1px solid black";
+//   }
+//
+//   function displayPublications(e){
+//     // alert("about")
+//     e.preventDefault();
+//     main = document.getElementById('main')
+//     main.innerHTML = "";
+//     main.innerHTML = "<h1>Publications</h1>";
+//   }
+//
+//   function displayBlog(e){
+//     // alert("about")
+//     e.preventDefault();
+//     main = document.getElementById('main')
+//     main.innerHTML = "";
+//     main.innerHTML = "<h1>Blog</h1>";
+//   }
+//
+//   function displayAbout(e){
+//     // alert("about")
+//     e.preventDefault();
+//     main = document.getElementById('main')
+//     main.innerHTML = "";
+//     main.innerHTML = "<h1>About</h1>";
+//   }
+//
+//   function displayHome(e){
+//     // alert("about")
+//     e.preventDefault();
+//     main = document.getElementById('main')
+//     main.innerHTML = "";
+//     main.innerHTML = "<h2>Main Section</h2>" +
+//             "<div class='scroll'>" +
+//               "<section class='post'>" +
+//                 "<h2>Post1</h2>" +
+//                 "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia corporis inventore molestias numquam sed, nihil, maxime expedita magni quo, atque, tempore voluptatibus ea. Magni velit quo error laudantium quos. Vitae?</p>" +
+//               "</section >" +
+//               "<section class='post'>" +
+//                 "<h2>Post2</h2>" +
+//                 "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae fugiat neque ratione veniam, architecto omnis rerum. Aliquam, enim nisi hic laboriosam, ipsam cum sapiente nostrum eos illum numquam, velit dolorum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia corporis inventore molestias numquam sed, nihil, maxime expedita magni quo, atque, tempore voluptatibus ea. Magni velit quo error laudantium quos. Vitae?</p>" +
+//               "</section>" +
+//               "<section class='post'>" +
+//                 "<h2>Post3</h2>" +
+//                 "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit in, ad eius ducimus laborum hic recusandae saepe. Nulla veniam, tempora harum laboriosam quae a, molestiae ducimus numquam ex, aperiam unde.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quibusdam quia iusto adipisci sunt! Laborum autem aliquam consequuntur qui nihil dolorem nostrum culpa labore dolorum omnis eos officiis, cum dolore.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia corporis inventore molestias numquam sed, nihil, maxime expedita magni quo, atque, tempore voluptatibus ea. Magni velit quo error laudantium quos. Vitae?</p>" +
+//               "</section>"+
+//           "</div>";
+//   }
+// }
